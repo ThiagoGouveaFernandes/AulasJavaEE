064dad031965ebf50889789bf323ddc80dce0e07
aluno <aluno@DSN1251230717>
2022-04-19 17:18:03 -0300

    d

---
+++ ExercicioSoma/src/main/java/br/com/senai/thiagogouvea/application/PageBean.java
@@ -0,0 +1,49 @@
+package br.com.senai.thiagogouvea.application;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.Flash;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class PageBean implements Serializable{
+	private double valor1;
+	private double valor2;
+	private double valor3;
+	
+	public double getValor1() {
+		return valor1;
+	}
+
+	public void setValor1(double valor1) {
+		this.valor1 = valor1;
+	}
+
+	public double getValor2() {
+		return valor2;
+	}
+
+	public void setValor2(double valor2) {
+		this.valor2 = valor2;
+	}
+
+	public double getValor3() {
+		return valor3;
+	}
+
+	public void setValor3(double valor3) {
+		this.valor3 = valor3;
+	}
+
+	@Inject
+	private Flash flash;
+	
+	public String calcular() {
+		flash.put("resultado", (valor1 + valor2 + valor3));
+		
+		return "result";
+	}
+}
