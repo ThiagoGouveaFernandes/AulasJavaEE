ba6cf83efac689ac90154c8ec3e8aab2a8f7a819
aluno <aluno@DSN1251230717>
2022-04-19 17:02:32 -0300

    c

---
+++ ProjetoEnvioDeDadosEntreTela/src/main/java/br/com/senai/thiagogouvea/application/PageBean.java
@@ -0,0 +1,31 @@
+package br.com.senai.thiagogouvea.application;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.Flash;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class PageBean implements Serializable{
+	private String nome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	@Inject
+	private Flash flash;
+	
+	public String processar() {
+		flash.put("nomeDoUsuario", nome);
+		
+		return "result?faces-redirect=true";
+	}
+}
