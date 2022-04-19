68500d5ec20506ba1489e7133e15b761d3683cb3
aluno <aluno@DSN1251230717>
2022-04-19 17:29:53 -0300

    e

---
+++ ExercicioInverterTexto/src/main/java/br/com/senai/thiagogouvea/application/PageBean.java
@@ -0,0 +1,31 @@
+package br.com.senai.thiagogouvea.application;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.context.Flash;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class PageBean implements Serializable{
+	private StringBuilder palavra;
+
+	public StringBuilder getPalavra() {
+		return palavra;
+	}
+
+	public void setPalavra(StringBuilder palavra) {
+		this.palavra = palavra;
+	}
+
+	@Inject
+	private Flash flash;
+	
+	public String inverter() {
+		flash.put("resultado", palavra.reverse());
+		
+		return "result";
+	}
+}
