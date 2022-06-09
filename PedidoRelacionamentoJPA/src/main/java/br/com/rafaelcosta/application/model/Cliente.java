24187c82452ce2d38f97d70012209aaa82267982
Thiago Gouvêa <102614703+ThiagoGouveaFernandes@users.noreply.github.com>
2022-06-09 14:32:21 -0300

    Update Cliente.java

--- PedidoRelacionamentoJPA/src/main/java/br/com/rafaelcosta/application/model/Cliente.java
+++ PedidoRelacionamentoJPA/src/main/java/br/com/rafaelcosta/application/model/Cliente.java
@@ -9,19 +9,23 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+//Essa anotação cria a tabela cliente no banco de dados
 @Entity
 public class Cliente implements Serializable {
 	
+	//Essas duas anotações atribuem um id a cada cliente automaticamente a cada cliente cadastrado
 	@Id
 	@GeneratedValue
 	private Integer id;
 	
+	//Essa anotação cria uma coluna na tabela cliente conforme as especificações juntas a ela
 	@Column(length = 100, nullable = false)
 	private String nome;
 	
 	@Column(length = 50, nullable = false)
 	private String email;
 
+	//Essa anotação é responsavel por determinar a cardinalidade entre duas tabelas
 	@OneToMany(mappedBy = "cliente")
 	private List<Pedido> pedidos;
 
@@ -57,6 +61,7 @@ public class Cliente implements Serializable {
 		this.pedidos = pedidos;
 	}
 
+	//Essa anotação é responsavel por sobrescrever o metodo
 	@Override
 	public String toString() {
 		return "Cliente [id=" + id + ", nome=" + nome + ", email=" + email + "]";
