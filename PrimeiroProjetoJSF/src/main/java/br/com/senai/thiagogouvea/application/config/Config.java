8c28920768d0bb6834bc176e372bce20734da9a6
Thiago Gouvêa <102614703+ThiagoGouveaFernandes@users.noreply.github.com>
2022-06-09 14:03:21 -0300

    Update Config.java

--- PrimeiroProjetoJSF/src/main/java/br/com/senai/thiagogouvea/application/config/Config.java
+++ PrimeiroProjetoJSF/src/main/java/br/com/senai/thiagogouvea/application/config/Config.java
@@ -4,7 +4,9 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.faces.annotation.FacesConfig;
 import javax.faces.annotation.FacesConfig.Version;
 
+//Força ao JSF usar esta versão em especifico
 @FacesConfig(version = Version.JSF_2_3)
+//Permite que essa classe funcione enquanto o servidor estiver funcionando
 @ApplicationScoped
 public class Config {
 
