b2975556e699dadc647b24ed64330325368c6ce6
Thiago Gouvêa <102614703+ThiagoGouveaFernandes@users.noreply.github.com>
2022-06-09 13:54:54 -0300

    Update PageBean.java

--- ExercicioSoma/src/main/java/br/com/senai/thiagogouvea/application/PageBean.java
+++ ExercicioSoma/src/main/java/br/com/senai/thiagogouvea/application/PageBean.java
@@ -7,13 +7,17 @@ import javax.faces.context.Flash;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+//Essa anotação nos possibilita determinar como chamaremos essa classe em outras classes ou arquivos xhtml
 @Named
+//Essa anotação permite que essa classe continue funcionando enquanto estiver sendo requisitada
 @RequestScoped
 public class PageBean implements Serializable{
+	//declarando variaveis privados
 	private double valor1;
 	private double valor2;
 	private double valor3;
 	
+	//Esse metodos getters e seters são responsaveis por pegar certo valor de uma variavel privada ou atribuir algum valor a ela
 	public double getValor1() {
 		return valor1;
 	}
@@ -37,10 +41,12 @@ public class PageBean implements Serializable{
 	public void setValor3(double valor3) {
 		this.valor3 = valor3;
 	}
-
+	
+	//Essa notação injeta codigos no java
 	@Inject
 	private Flash flash;
 	
+	//É metodo de soma que utiliza recursos do flash
 	public String calcular() {
 		flash.put("resultado", (valor1 + valor2 + valor3));
 		
