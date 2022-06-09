3de855c0a30ff6c7ea28f7f7db3146fef216a388
Thiago Gouvêa <102614703+ThiagoGouveaFernandes@users.noreply.github.com>
2022-06-09 15:53:02 -0300

    Update TabelaBean.java

--- TabelaDinamica2K20/src/main/java/br/com/senai/thiagogouvea/application/bean/TabelaBean.java
+++ TabelaDinamica2K20/src/main/java/br/com/senai/thiagogouvea/application/bean/TabelaBean.java
@@ -14,8 +14,10 @@ import br.com.senai.thiagogouvea.application.model.Despesas;
 @Named("tabela")
 @SessionScoped
 public class TabelaBean implements Serializable{
+	//Criando a lista de despesas
 	private List<Despesas> despesas = new ArrayList<>();
 	
+	//Metodo responsavel por inserir novas despesas na lista
 	public String inserir() {
 		Despesas d = new Despesas();
 		despesas.add(d);
@@ -23,21 +25,25 @@ public class TabelaBean implements Serializable{
 		return null;
 	}
 	
+	//Metodo responsavel por excluir as despesas da lista
 	public String excluir(Despesas despesa) {
 		despesas.remove(despesa);
 		return null;
 	}
 	
+	//Metodo responsavel por editar os dados que estão na lista
 	public String editar(Despesas despesa) {
 		despesa.setEdit(true);
 		return null;
 	}
 	
+	//Metodo responsavel por gravar as novas informações da tabela
 	public String gravar(Despesas despesa) {
 		despesa.setEdit(false);
 		return null;
 	}
 	
+	//Metodo responsavel por retornar a lista de despesas
 	public List<Despesas> getDespesas() {
 		return despesas;
 	}
